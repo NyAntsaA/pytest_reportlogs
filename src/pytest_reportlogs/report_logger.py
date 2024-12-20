1079649bfff6dcbff9baea04b52ebc5562986572
nyandriantsoa <nyandriantsoa@gmail.com>
2024-12-20 10:15:51 +0100

    Add "silent" logger to display useful info in terminals
        Useful information is displayed in terminals
        but not reported in the final pytest report

--- src/pytest_reportlogs/report_logger.py
+++ src/pytest_reportlogs/report_logger.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+REPORTLOGS_LOGGER_NAME = "pytest_reportlogs"
+MAX_LOG_LENGTH = 80
+STEP_PASSED_MARKER = "- [OK]"
+
+
 class Singleton(object):
     def __new__(cls):
         if not hasattr(cls, "instance"):
@@ -8,12 +18,58 @@ class Singleton(object):
 class ReportLogger(Singleton):
     def __init__(self):
         self._logs = []
+        self._steps_counter = 0
+        self._logger = logging.getLogger(REPORTLOGS_LOGGER_NAME)
 
     def report_log(self, msg: str, is_step: bool = True):
-        self._logs.append((msg, is_step))
+        """
+        API for reporting useful information
+        This is the only interface to be used in the tests
+
+        Parameters
+        ----------
+        msg: str
+            the useful information to be reported
+
+        is_step: bool, optional
+            set it to
+             - True if the information should be treated as a test step
+             - False if a simple useful information 
+            default: True
+
+        """
+        formatted_msg = self._log_formatter(msg, is_step)
+        self._logs.append(formatted_msg)
+        self._logger.info(formatted_msg.strip())
 
     def get_logs(self):
         return self._logs
 
     def clear_logs(self):
         self._logs = []
+        self._steps_counter = 0
+
+    def _log_formatter(self, msg, is_step=False):
+        """
+        Make logs pretty
+            - steps logs are prefixed by `[step #]`
+            - steps logs are formatted to be 80-bytes long max
+        """
+
+        formatted_msg = ""
+        info_prefix = "[  INFO>  ] "
+        multiline_info_prefix = " " * (len(info_prefix) + 2)
+
+        if is_step:
+            self._steps_counter += 1
+            formatted_msg += f"[STEP #{self._steps_counter:03d}] {msg} "
+            formatted_msg += "-" * (MAX_LOG_LENGTH - len(STEP_PASSED_MARKER))
+            formatted_msg = formatted_msg[: (MAX_LOG_LENGTH - len(STEP_PASSED_MARKER))]
+            formatted_msg += STEP_PASSED_MARKER + "\n"
+        else:
+            for j, m in enumerate(msg.splitlines()):
+                # prefix = "    " if j else "> "
+                prefix = multiline_info_prefix if j else info_prefix
+                formatted_msg += f"{prefix}{m}\n"
+
+        return formatted_msg
--- tests/test_pytest_reportlogs.py
+++ tests/test_pytest_reportlogs.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from pytest_reportlogs import report_log
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("TestLogger")
 
 
 def test_steps_formatting():
     """Test short and long steps are correctly formatted"""
+
+    logger.info("Starting test test_steps_formatting")
+
     a = b = 0
     assert a == b
     report_log("Simple test step description")
@@ -14,8 +22,11 @@ def test_steps_formatting():
     report_log("Very long test step description" + "* " * 100)
 
 
-def test_general_log_formatting():
+def test_useful_info_formatting():
     """Test short and long useful information are correctly formatted"""
+
+    logger.info("Starting test test_useful_info_formatting")
+
     a = b = 0
     assert a == b
     report_log("Simple useful information", False)
@@ -26,15 +37,18 @@ def test_general_log_formatting():
 
     a = b = 2
     assert a == b
-    multiline_info = """This is a multiline information:
-This is the first information.
-This is another information.
+    multiline_info = """Multiline information:
+This is the first line.
+This is another line.
 """
     report_log(multiline_info, False)
 
 
-def test_mixed_steps_and_general_logs():
+def test_mixed_steps_and_useful_info():
     """Test steps and useful information used together"""
+
+    logger.info("Starting test test_mixed_steps_and_useful_info")
+
     a = b = 0
     assert a == b
     report_log("Some test step description")
